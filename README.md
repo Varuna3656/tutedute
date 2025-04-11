10bbd7a746fc59e19e40868b49a9c616843ff6b6
varuna-naik <Varuna3656@gmail.com>
2025-04-11 16:19:18 +0530

    Assignment 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Task 1
+taken two integer numbers and performed basic arithmetic operations and displayed results
+
+Task 2
+taken first name and last name of the user as fname and lname. Printed a greet message with their full name
---
+++ task1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+sum = a+b
+diff = a-b
+product = a*b
+quotient = a/b
+
+print("Addition: ",sum)
+print("Subtraction: ",diff)
+print("Multiplication: ",product)
+print("Division: ",quotient)
